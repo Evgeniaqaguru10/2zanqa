e34e9520439fe6db436ff8137dc287108868a8b0
ydzhincode <ydzingit@yandex.ru>
2024-01-22 14:52:54 +0300

    добавили строчку в редми из пайчарм указали зависимости проекта (окружение)

---
+++ test_dz2.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture
+def settings():
+    browser.config.window_width = 1020
+    browser.config.window_width = 1500
+    yield
+    browser.quit()
+
+
+def test_selene(settings):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_empty_google(settings):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('hklkjhfd').press_enter()
+    browser.element('[id="center_col"]').should(have.text(' hklkjhfd не найдено.'))
